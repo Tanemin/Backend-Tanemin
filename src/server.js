c8028eb2d7df4023246130e959da5dc88763588e
Irwan Gumilar <irwangumilar111@gmail.com>
2022-05-30 19:20:58 +0700

    Updating module package

---
+++ src/server.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config();
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPOTION: Shutting down...');
+  console.log(err.name, err.message);
+  // server.close(() => {
+  process.exit(1);
+  // });
+});
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION: Shutting down...');
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
